aad123537a4160572b5e23f3941e163af882078a
Nathaniel Hall <nathaniel.hall@gmail.com>
2019-11-10 19:43:56 -0500

    rename

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,14 @@ type DogApi = {
   message: string
 }
 export const App: FC = () => {
-  const [getUsers] = useApi<DogApi>('breeds/image/random')
+  const [getDog] = useApi<DogApi>('breeds/image/random')
 
-  if (getUsers.loading) return <h2>Loading</h2>
-  if (getUsers.error) return <h2>'Error'</h2>
+  if (getDog.loading) return <h2>Loading</h2>
+  if (getDog.error) return <h2>'Error'</h2>
 
   return (
     <>
-      {getUsers.response && (
-        <img src={getUsers.response.data.message} alt="new" />
-      )}
+      {getDog.response && <img src={getDog.response.data.message} alt="new" />}
     </>
   )
 }
